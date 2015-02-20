39a9be0e7dca795dd04409a5d8d23a3f9ef9ff81
Leandro Segovia <ldlsegovia@gmail.com>
2015-02-20 15:23:49 -0300

    feat(): add live demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,46 @@
-var app = angular.module('example', ['restmod', 'PlAuth']);
-app.factory('Note', function(restmod) {
-  return restmod.model('http://private-19413-angularauthapi.apiary-mock.com/notes');
+var app = angular.module('example', ['restmod', 'PlAuth']).config(function(AuthConfigProvider){
+  AuthConfigProvider.setUidKey('email');
+  AuthConfigProvider.setTokenKey('auth_token');
 });
-app.controller('ExampleCtrl', function(Note) {
+
+var apiUrl = 'http://private-19413-angularauthapi.apiary-mock.com';
+
+app.factory('Note', function(restmod) {
+  return restmod.model(apiUrl + '/notes').mix('HttpAuth');
+});
+
+app.factory('User', function(restmod) {
+  return restmod.model(apiUrl + '/users').mix('HttpAuth');
+});
+
+app.factory('Session', function(restmod) {
+  return restmod.model(apiUrl + '/sessions').mix('HttpAuth');
+});
+
+app.controller('ExampleCtrl', function(Session, User, Note, AuthSrv) {
   var vm = this;
-  vm.notes = Note.$search();
+  vm.register = register;
+  vm.login = login;
+  vm.logout = logout;
+  vm.authSrv = AuthSrv;
+  vm.getNotes = getNotes;
+  vm.notes = Note.$collection();
+
+  function register() {
+    var payload = {email: 'charles@manson.com', password: '12345678', password_confirmation: '12345678'};
+    User.$create(payload).$then(AuthSrv.store);
+  }
+
+  function login() {
+    var payload = {email: 'charles@manson.com', password: '12345678'};
+    Session.$create(payload).$then(AuthSrv.store);
+  }
+
+  function logout() {
+    Session.$new('current').$destroy().$then(AuthSrv.clear);
+  }
+
+  function getNotes() {
+    vm.notes.$fetch();
+  }
 });
