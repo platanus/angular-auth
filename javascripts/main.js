0fef0f55e5cc0d79fb76d260ecab166fd75f09e2
Emilio Blanco <emilioeduardob@gmail.com>
2015-02-20 14:31:35 -0300

    chore(): Setup angular and library

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+var app = angular.module('example', ['restmod', 'PlAuth']);
+app.factory('Note', function(restmod) {
+  return restmod.model('http://private-19413-angularauthapi.apiary-mock.com/notes');
+});
+app.controller('ExampleCtrl', function(Note) {
+  var vm = this;
+  vm.notes = Note.$search();
+});
