7187e3e7b509cfea642a50590df9b4e0aebf285e
Leandro Segovia <ldlsegovia@gmail.com>
2015-08-28 16:51:31 -0300

    feat(): use local storage prefix to set keys

--- src/local-data-srv.js
+++ src/local-data-srv.js
@@ -2,21 +2,29 @@
   'use strict';
 
   angular.module('PlAuth')
-  .factory('LocalDataSrv', function() {
+  .factory('LocalDataSrv', LocalDataSrv);
+
+  LocalDataSrv.$inject = ['AuthConfig'];
+
+  function LocalDataSrv(AuthConfig) {
+    function prefixedKey(_key) {
+      return [AuthConfig.getLocalStoragePrefix(), _key].join('.');
+    }
+
     var eraseKey = function(_key) {
-      delete window.localStorage[_key];
+      delete window.localStorage[prefixedKey(_key)];
     };
 
     var getKey = function(_key) {
-      return localStorage.getItem(_key);
+      return localStorage.getItem(prefixedKey(_key));
     };
 
     var isKeySet = function(_key) {
-      return !!localStorage.getItem(_key);
+      return !!localStorage.getItem(prefixedKey(_key));
     };
 
     var setKey = function(_key, _value) {
-      localStorage.setItem(_key, _value);
+      localStorage.setItem(prefixedKey(_key), _value);
     };
 
     return {
@@ -25,6 +33,6 @@
       isKeySet: isKeySet,
       eraseKey: eraseKey
     };
-  });
+  }
 
 })();
